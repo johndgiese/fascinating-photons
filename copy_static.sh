27a288747af84955de7279f229f9a59c897b809f
johndgiese <johndgiese@gmail.com>
2013-09-25 13:55:47 -0400

    Change how static files are served (for use with gh-pages)

---
+++ copy_static.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp ./vendor/jquery/jquery.min.js ./js/jquery.min.js
+cp ./vendor/bootstrap/dist/js/bootstrap.min.js ./js/bootstrap.min.js 
+cp ./vendor/bootstrap/dist/css/bootstrap.min.css ./css/bootstrap.min.css
+cp ./vendor/MathJax/MathJax.js ./js/MathJax.js
